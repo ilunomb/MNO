c53635a0cafc3504a2854255117e2b9cabebe0d1
Ilan Ezequiel Nomberg <91914751+ilunomb@users.noreply.github.com>
2024-03-20 20:21:47 -0300

    Add files via upload

---
+++ Tp1/ej1_fa_Pchip.py
@@ -0,0 +1,75 @@
+import numpy as np
+from scipy.interpolate import PchipInterpolator
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Definición de las funciones
+def fa(x):
+    return 0.3 ** (np.abs(x)) * np.sin(4*x) - np.tanh(2*x) + 2
+
+# Puntos de colocación equiespaciados // orden del polinomio
+quantity_of_interpolation_points = 10
+
+x_values_for_interpolation = np.linspace(-4, 4, quantity_of_interpolation_points)
+
+# puntos a graficar de las funciones
+points_to_graph = np.linspace(-4, 4, 1000)
+
+# Puntos de colocación Chebyshev // orden del polinomio
+quantity_of_interpolation_points_chebyshev = 10
+
+# Generar nodos de Chebyshev en el intervalo [-4, 4]
+cheb_nodes = np.polynomial.chebyshev.chebpts1(quantity_of_interpolation_points_chebyshev)
+x_values_for_interpolation_chebyshev = 4 * cheb_nodes  # Escalamos los nodos de Chebyshev al intervalo [-4, 4]
+
+# Evaluación de las funciones en los puntos de colocación
+fa_values = fa(x_values_for_interpolation)
+fCheb_values = fa(x_values_for_interpolation_chebyshev)
+
+# Interpolación utilizando Lagrange para fa(x)
+fa_interp = PchipInterpolator(x_values_for_interpolation, fa_values)
+fCheb_interp = PchipInterpolator(x_values_for_interpolation_chebyshev, fCheb_values)
+
+# Gráficos de las funciones originales y las interpoladas
+plt.figure(figsize=(12, 5))
+
+plt.subplot(2, 2, 1)
+plt.scatter(x_values_for_interpolation, fa_values, label="Puntos de interpolacion equiespaciados", color='g')
+plt.plot(points_to_graph, fa(points_to_graph), label='Datos originales', color='r')
+plt.plot(points_to_graph, fa_interp(points_to_graph), label='Interpolación',  linestyle='-.')
+plt.title('Interpolación de $f_a(x)$ con Pchip')
+plt.xlabel('x')
+plt.ylabel('$f_a(x)$')
+plt.legend()
+plt.grid()
+
+plt.subplot(2, 2, 2)
+plt.scatter(x_values_for_interpolation_chebyshev, fCheb_values, label="Puntos de interpolacion no equiespaciados", color='g')
+plt.plot(points_to_graph, fa(points_to_graph), label='Datos originales', color='r')
+plt.plot(points_to_graph, fCheb_interp(points_to_graph), label='Interpolación',  linestyle='-.')
+plt.title('Interpolación de $f_a(x)$ con Pchip (Chebyshev)')
+plt.xlabel('x')
+plt.ylabel('$f_a(x)$')
+plt.legend()
+plt.grid()
+
+# Gráficos de los errores de interpolación
+plt.subplot(2, 2, 3)
+plt.plot(points_to_graph, np.abs(fa(points_to_graph) - fa_interp(points_to_graph)), label='Error equiespaciados', color='r')
+plt.title('Error de interpolación de $f_a(x)$ con Pchip')
+plt.xlabel('x')
+plt.ylabel('Error')
+plt.legend()
+plt.grid()
+
+
+plt.subplot(2, 2, 4)
+plt.plot(points_to_graph, np.abs(fa(points_to_graph) - fCheb_interp(points_to_graph)), label='Error no equiespaciados', color='r')
+plt.title('Error de interpolación de $f_a(x)$ con Pchip (Chebyshev)')
+plt.xlabel('x')
+plt.ylabel('Error')
+plt.legend()
+plt.grid()
+
+plt.tight_layout()
+plt.show()
---
+++ Tp1/ej2_a_lagrange.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import matplotlib.pyplot as plt  # Importar librería para graficar
+import numpy as np
+from scipy.interpolate import lagrange
+
+#read CSV with pandas
+ground_truth_df = pd.read_csv("mnyo_ground_truth.csv", sep=" ", header=None, names=["x1", "x2"])
+
+mediciones_df = pd.read_csv("mnyo_mediciones.csv", sep=" ", header=None, names=["x1", "x2"])
+
+
+#interpolate the trajectory based on index
+interpolated_trajectory_x1 = lagrange(mediciones_df.index, mediciones_df["x1"])
+interpolated_trajectory_x2 = lagrange(mediciones_df.index, mediciones_df["x2"])
+
+
+#generate 1000 points to graph
+points_to_graph = np.linspace(mediciones_df.index.min(), mediciones_df.index.max(), 1000)
+
+
+#plot init
+plt.figure(figsize=(12, 5))
+
+#plot the ground truth
+plt.plot(ground_truth_df["x1"], ground_truth_df["x2"], label='Ground Truth', color='b')
+
+#plot the interpolated trajectory
+plt.plot(interpolated_trajectory_x1(points_to_graph), interpolated_trajectory_x2(points_to_graph), label='Interpolation' ,linestyle='-.', color='r')
+
+#plot the points
+plt.scatter(mediciones_df["x1"], mediciones_df["x2"], label="Mediciones", color='g')
+
+plt.title('Trajectory Plot')
+plt.xlabel('X Position')
+plt.ylabel('Y Position')
+
+plt.legend()
+plt.show()
